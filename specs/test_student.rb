104fe53877886a6a78d668a750d69b2838f8f172
annalanigan <annalanigan@outlook.com>
2017-11-06 16:05:52 +0000

    student class tests passed

--- specs/test_student.rb
+++ specs/test_student.rb
@@ -15,8 +15,27 @@ class TestStudent < MiniTest::Test
     assert_equal(17, class_student.cohort)
   end
 
-  # def test_set_student_name
-  #   class_student =
-  # end
+  def test_set_student_name
+    class_student = Student.new("Anna", 17)
+    class_student.name = "Bob"
+    assert_equal("Bob", class_student.name)
+  end
+
+  def test_set_student_cohort
+    class_student = Student.new("Anna", 17)
+    class_student.cohort = 16
+    assert_equal(16, class_student.cohort)
+  end
+
+  def test_student_talk
+    class_student = Student.new("Anna", 17)
+    assert_equal("I can talk!", class_student.talk)
+  end
+
+  def test_favourite_language
+    class_student = Student.new("Anna", 17)
+    result = class_student.favourite_language("Ruby")
+    assert_equal("I love Ruby", result)
+  end
 
 end
--- student.rb
+++ student.rb
@@ -1,9 +1,17 @@
 class Student
-  attr_reader :name, :cohort
+  attr_accessor :name, :cohort
 
-def initialize(name, cohort)
-  @name = name
-  @cohort = cohort
-end
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def talk
+    return "I can talk!"
+  end
+
+  def favourite_language(language)
+    return "I love #{language}"
+  end
 
 end
